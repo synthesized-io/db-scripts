ef7d9a0ac761d00df91354b6431e517f0a7847bf
mgramin <mgramin@gmail.com>
2023-11-23 17:48:08 +0300

    Update

---
+++ sql/sqlserver/rules.sql
@@ -0,0 +1,6 @@
+select
+    name as rule_name,
+    syscomments.text as rule_definition
+from sysobjects
+inner join syscomments on sysobjects.id = syscomments.id
+where sysobjects.xtype = 'R';
--- sql/sqlserver/user_defined_data_types.sql
+++ sql/sqlserver/user_defined_data_types.sql
@@ -6,7 +6,8 @@ select
     u.max_length,
     u.is_nullable,
     u.is_assembly_type,
-    u.is_table_type
+    u.is_table_type,
+    u.rule_object_id
 from sys.types as u
 inner join sys.types as s on u.system_type_id = s.user_type_id
 where u.is_user_defined = 1;
