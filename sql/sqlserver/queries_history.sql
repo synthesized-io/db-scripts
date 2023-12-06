52a393f54ce102ed867250fc000035dfc8608a27
mgramin <mgramin@gmail.com>
2023-12-06 16:37:29 +0300

    Add queries history

---
+++ sql/sqlserver/queries_history.sql
@@ -0,0 +1,8 @@
+select stats.last_execution_time,
+  stats.execution_count,
+  stats.total_rows,
+  stats.last_rows,
+  s.text
+from sys.dm_exec_query_stats stats
+cross apply sys.dm_exec_sql_text(stats.sql_handle) s
+order by last_execution_time desc;
