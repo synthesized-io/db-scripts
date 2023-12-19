d320d2322ec473d61d9054f076328845b4dbe489
mgramin <mgramin@gmail.com>
2023-12-19 15:05:29 +0300

    Update

--- sql/sqlserver/columns.sql
+++ sql/sqlserver/columns.sql
@@ -1,4 +1,5 @@
 select
+    schema_name(tab.schema_id) as schema_name,
     tab.name as table_name,
     c.name as column_name,
     t.name as type_name,
@@ -6,11 +7,11 @@ select
     c.max_length,
     c.precision,
     c.scale,
-    c.is_nullable,
-    schema_name(tab.schema_id) as schema_name
+    c.is_nullable
 from sys.columns as c
 inner join
     sys.tables as tab
     on tab.object_id = c.object_id and tab.is_ms_shipped = 0
 left join sys.types as t on c.user_type_id = t.user_type_id
-left join sys.types as bt on t.system_type_id = bt.user_type_id;
+left join sys.types as bt on t.system_type_id = bt.user_type_id
+order by schema_name(tab.schema_id), tab.name;
--- sql/sqlserver/constraints.sql
+++ sql/sqlserver/constraints.sql
@@ -1,8 +1,25 @@
 select
-    type,
-    type_desc,
-    is_ms_shipped,
-    is_disabled,
-    definition,
-    is_system_named
-from sys.check_constraints;
+    t.name as table_name,
+    col.name as column_name,
+    t.type,
+    t.type_desc,
+    c.type as constraint_type,
+    c.type_desc as constraint_type_desc,
+    c.name as constraint_name,
+    i.name as index_name,
+    i.type as index_type,
+    i.type_desc as index_type_desc,
+    schema_name(t.schema_id) as schema_name
+from sys.objects as t
+inner join sys.indexes as i on t.object_id = i.object_id
+inner join
+    sys.key_constraints as c
+    on i.object_id = c.parent_object_id and i.index_id = c.unique_index_id
+left join
+    sys.index_columns as ic
+    on ic.object_id = t.object_id and ic.index_id = i.index_id
+left join
+    sys.columns as col
+    on ic.object_id = col.object_id and ic.column_id = col.column_id
+where t.is_ms_shipped = 0
+order by schema_name(t.schema_id), t.name, col.name;
