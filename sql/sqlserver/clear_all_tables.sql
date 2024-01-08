5e33797db67f1350d58dbf2a096beae1d47d9afa
mgramin <mgramin@gmail.com>
2024-01-08 18:59:16 +0300

    Delete dangerous scripts

--- sql/sqlserver/clear_all_tables.sql
+++
@@ -1,8 +0,0 @@
--- Disable foreign key constraints in dbo schema
-EXEC sp_MSforeachtable "IF OBJECT_SCHEMA_NAME(object_id('?')) = 'dbo' BEGIN ALTER TABLE ? NOCHECK CONSTRAINT all END"
-
--- Delete all data from tables in dbo schema
-EXEC sp_MSforeachtable "IF OBJECT_SCHEMA_NAME(object_id('?')) = 'dbo' BEGIN DELETE FROM ? END"
-
--- Re-enable foreign key constraints in dbo schema
-EXEC sp_MSforeachtable "IF OBJECT_SCHEMA_NAME(object_id('?')) = 'dbo' BEGIN ALTER TABLE ? WITH CHECK CHECK CONSTRAINT ALL END"
