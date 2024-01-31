2f38e35ab3e0a64899bac6787effdc79266ad504
mgramin <mgramin@gmail.com>
2024-01-31 11:40:20 +0300

    Add index columns

--- sql/sqlserver/indexes.sql
+++ sql/sqlserver/indexes.sql
@@ -1,6 +1,10 @@
 select
     t.name as table_name,
     i.name as index_name,
+    col.name as column_name,
+    ic.index_id as column_index_id,
+    ic.index_column_id as column_index_column_id,
+    ic.is_descending_key as column_is_descending_key,
     i.is_disabled,
     i.auto_created,
     i.type,
@@ -14,5 +18,9 @@ select
     schema_name(t.schema_id) as schema_name
 from sys.indexes as i
 inner join sys.tables as t on t.object_id = i.object_id and t.is_ms_shipped != 1
+inner join 
+     sys.index_columns ic ON  i.object_id = ic.object_id and i.index_id = ic.index_id 
+inner join 
+     sys.columns col ON ic.object_id = col.object_id and ic.column_id = col.column_id 
 where i.type > 0
 order by schema_name(t.schema_id), t.name, i.name;
